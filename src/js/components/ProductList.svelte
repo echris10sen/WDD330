7a81ed8e9ce5aba22f05a497dbe457b423cc5559
Josh Skouson <Jmatt6836@gmail.com>
2023-10-05 11:15:50 -0600

    team activity 4 Co-authored-by: Daniel Carr <DCPenguin73@users.noreply.github.com> Co-authored-by: Christensen, Eric <chr15016@byui.edu> Co-authored-by: Tsosie, Everett <SgtTacos@users.noreply.github.com>

---
+++ src/js/components/ProductList.svelte
@@ -0,0 +1,28 @@
+<script>
+import ProductSummary from "./ProductSummary.svelte";
+import { getData } from "../productData.mjs";
+
+// this is how we make a prop in svelte
+export let category;
+// if you are looking at this thinking that's strange to just stop with a promise
+// you would be right.  This will make more sense in a bit...stay tuned.
+let promise = getData(category);
+</script>
+
+<h2>Top products: {category}</h2>
+{#await promise}
+    Loading
+{:then data}
+    <ul class="product-list">
+        {#each  data  as product}
+
+            <li class="product-card">
+                <ProductSummary {product}/>
+            </li>
+        {/each}
+    </ul>
+{/await}
+  
+  
+
+  
