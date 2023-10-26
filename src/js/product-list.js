a9212ed31abb6fa5e05f07c7cec651a290f2898a
echris10sen <chr15016@byui.edu>
2023-10-25 21:13:38 -0600

    Co-authored-by: Tsosie, Everett <SgtTacos@users.noreply.github.com> Co-authored-by: Daniel Carr <DCPenguin73@users.noreply.github.com>

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,6 +1,10 @@
 import ProductList from "./components/ProductList.svelte";
+// import { renderHeaderFooter } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
+
+// renderHeaderFooter();
 
 new ProductList({
     target: document.querySelector(".products"),
-    props: { category: "tents" },
+    props: { category: getParam("filter") }
 });
