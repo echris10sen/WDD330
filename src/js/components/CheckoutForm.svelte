403649827f7a66f89ba36b84d06c96c7857c6413
Daniel Carr <DBCarr980@gmail.com>
2023-11-07 10:46:59 -0700

    refactor oder summary Co-authored-by: Tsosie, Everett <SgtTacos@users.noreply.github.com>

--- src/js/components/CheckoutForm.svelte
+++ src/js/components/CheckoutForm.svelte
@@ -26,18 +26,28 @@
         list = getLocalStorage("so-cart");
         calculateItemSummary()
     }
+    function packageItems(items){
+        // convert the list of products from localStorage to the simpler 
+        //       form required for the checkout process. Array.map would be perfect for this.
+    }
+    function handleSubmit(e) {
+        // build the data object from the calculated fields, the items in the cart, and the information entered into the form
+        // remember that the form that was submitted can be found two ways...this or e.target 
+        // call the checkout method in our externalServices module and send it our data object.
+    }
     init();
     calculateOrdertotal();
 </script>
 
 <h2>Review & Place your Order</h2>
 <fieldset class="shipping">
+    <!-- need names: orderDate, items, orderTotal, shipping, tax -->
     <legend>Shipping</legend>
     <label for="firstName">First Name</label>
-    <input type="text" name="firstName"id="firstName">
+    <input type="text" name="fname"id="firstName">
     
     <label for="lastName">Last Name</label>
-    <input type="text" name="lastName" id="lastName">
+    <input type="text" name="lname" id="lastName">
     
     <label for="street">Street</label>
     <input type="text" name="street" id="street">
@@ -53,21 +63,37 @@
 </fieldset>
 <fieldset class="payment">
     <label for="cardNumber">Card Number</label>
-    <input type="text" id="cardNumber">
+    <input type="text" name="cardNumber" id="cardNumber">
     
     <label for="expiration">Expiration</label>
-    <input type="text" id="expiration">
+    <input type="text" name="expiration" id="expiration">
     
     <label for="securityCode">Security Code</label>
-    <input type="text" id="securityCode">
+    <input type="text" name="code" id="securityCode">
 </fieldset>
 
 <fieldset class="checkout-summary">
     <legend>Order Summary</legend>
-    <p>Item Subtotal({list.length}):         ${itemTotal}</p>
-    <p>Shipping Estimate:               ${shipping}</p>
-    <p>Tax:                             ${tax}</p>
-    <p>Order Total:                     ${orderTotal}<p>
+    <ul>
+        <li>
+            <label for="cartTotal">Item Subtotal({list.length})</label>
+            <p name="cartTotal" id="cartTotal">${itemTotal}</p>
+        </li>
+        <li>
+            <label for="shipping">Shipping Estimate</label>
+            <p name="shipping" id="shipping">${shipping}</p>
+        </li>
+        <li>
+            <label for="tax">Tax</label>
+            <p name="tax" id="tax">${tax}</p>
+        </li>
+        <li>
+            <label for="orderTotal"><b>Order Total</b></label>
+            <p name="orderTotal" id="orderTotal">${orderTotal}</p>
+        </li>
+    </ul>
+    <button id="checkoutSubmit" type="submit">Checkout</button>
+    
     
 </fieldset>
 
